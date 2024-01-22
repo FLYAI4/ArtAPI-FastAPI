9c86ae7c56ada12d9e91b50235de7a60b65850ad
robert-min <robertmin522@gmail.com>
2024-01-22 10:51:45 +0900

    feature: ADD test scenario

--- tests/apps/test_repository.py
+++ tests/apps/test_repository.py
@@ -15,7 +15,7 @@ async def test_user_repository_can_insert_data_with_valid():
 async def test_user_repository_cannot_insert_data_with_infalid():
     # give : 유요한 데이터(사용자 정보 + 이미지 정보)
 
-    # when : DB에 데이터 저장 시  DB연결 오류
+    # when : DB에 데이터 저장 시 DB연결 오류
 
     # then : 에러메시지 반환
     pass

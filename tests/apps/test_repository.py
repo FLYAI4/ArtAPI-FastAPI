36e21e555a1c788f4679739e64735578dc344f39
robert-min <robertmin522@gmail.com>
2024-01-22 10:49:18 +0900

    feature: ADD test scenario

---
+++ tests/apps/test_repository.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_user_repository_can_insert_data_with_valid():
+    # given : 유요한 데이터(사용자 정보 + 이미지 정보)
+
+    # when : DB에 데이터 저장
+
+    # then : 사용자 정보 + 이미지 경로 반환
+    pass
+
+
+@pytest.mark.asyncio
+async def test_user_repository_cannot_insert_data_with_infalid():
+    # give : 유요한 데이터(사용자 정보 + 이미지 정보)
+
+    # when : DB에 데이터 저장 시  DB연결 오류
+
+    # then : 에러메시지 반환
+    pass

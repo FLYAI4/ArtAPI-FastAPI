7f7ae9cf28e263bb754951a74fd1036653ccd9b4
robert-min <robertmin522@gmail.com>
2024-01-29 17:07:32 +0900

    test: ADD account repository get_all_user_account, delete_user_account

--- tests/apps/account/test_repository.py
+++ tests/apps/account/test_repository.py
@@ -1,8 +1,9 @@
 import pytest
+from sqlalchemy import select
+from src.apps.account.model import Account
 from src.libs.db_manager import PostgreManager
 from src.libs.exception import DBError
 from src.apps.account.repository import AccountRepository
-
 
 # Mock data
 EMAIL = "test@naver.com"
@@ -58,34 +59,53 @@ async def test_account_repository_cannot_get_user_account(session):
     with pytest.raises(DBError):
         AccountRepository.get_user_account(session, WRONG_EMAIL)
 
+# TODO : update 부분 나중에 개발
+# @pytest.mark.order(2)
+# @pytest.mark.asyncio
+# async def test_account_repository_can_update_user_account():
+#     # given : 변경할 유저 정보
+
+#     # when : DB에 데이터 변경 요청
+
+#     # then : 변경된 데이터 확인
+#     pass
+
 
 @pytest.mark.order(2)
 @pytest.mark.asyncio
-async def test_account_repository_can_update_user_account():
-    # given : 변경할 유저 정보(ID제외)
-
-    # when : DB에 데이터 변경 요청
-
-    # then : 변경된 데이터 확인
-    pass
-
-
-@pytest.mark.order(2)
-@pytest.mark.asyncio
-async def test_account_respository_can_get_all_user_account():
+async def test_account_respository_can_get_all_user_account(session):
     # given :
-
     # when : DB에 데이터 전체 조회 요청
+    with session:
+        all_account_email = list()
+        sql = select(Account)
+        for obj in session.execute(sql):
+            all_account_email.append(obj.Account.email)
 
     # then : 조회된 데이터 확인
-    pass
+    assert len(all_account_email) > 0
+    assert EMAIL in all_account_email
 
 
 @pytest.mark.asyncio(3)
-async def test_account_repository_cannot_get_all_user_account():
+async def test_account_repository_cannot_get_all_user_account(session):
     # given : 테이블에 아무 데이터도 없을 때
+    with session:
+        # given : DB에 기존 데이터
+        sql = select(Account).filter(Account.email == EMAIL)
+        # when : 기존 데이터 삭제
+        obj = session.execute(sql).scalar_one()
+        if obj:
+            session.delete(obj)
+        session.commit()
+        # then : email
 
-    # when : DB 데이터 조회 요청
+    # when : DB 데이터 전체 조회 요청
+    with session:
+        all_account_email = list()
+        sql = select(Account)
+        for obj in session.execute(sql):
+            all_account_email.append(obj.Account.email)
 
     # then : 빈 리스트 출력
-    pass
+    assert all_account_email == []

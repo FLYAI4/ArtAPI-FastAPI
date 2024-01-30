271c2d7c9964b16d235e7cbeebc3d753fbaba340
robert-min <robertmin522@gmail.com>
2024-01-30 11:01:18 +0900

    feature: ADD libs/cipher encrypt_password,decrypt_password.

---
+++ src/libs/cipher.py
@@ -0,0 +1,21 @@
+import os
+import base64
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+
+class CipherManager:
+    def __init__(self) -> None:
+        dek = bytes(os.environ.get('DEK_KEY'), 'utf-8')
+        self.BLOCK_SIZE = 16
+        self.aes = AES.new(dek, AES.MODE_ECB)
+    
+    def encrypt_password(self, origin_pw) -> bytes:
+        encoding_pw = pad(origin_pw.encode(), self.BLOCK_SIZE)
+        encrypt_pw = self.aes.encrypt(encoding_pw)
+        return base64.b64encode(encrypt_pw)
+
+    def decrypt_password(self, encrypt_pw) -> str:
+        decrypt_pw = self.aes.decrypt(base64.b64decode(encrypt_pw))
+        origin_pw = unpad(decrypt_pw, self.BLOCK_SIZE).decode()
+        return origin_pw
--- tests/libs/test_encrypt.py
+++ tests/libs/test_encrypt.py
@@ -1,45 +1,22 @@
-import os
-import json
 import pytest
-import base64
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
+from src.libs.cipher import CipherManager
 
 # Mock data
 ORI_PASSWORD = "rlaalswns1234"
-
-libs_path = os.path.abspath(os.path.join(__file__, os.path.pardir))
-tests_path = os.path.abspath(os.path.join(libs_path, os.path.pardir))
-apps_path = os.path.abspath(os.path.join(tests_path, 'apps'))
-conf_path = os.path.abspath(os.path.join(apps_path, 'conf'))
-conf_file = os.path.abspath(os.path.join(conf_path, 'conf.json'))
-
-with open(conf_file, "rt") as f:
-    conf = json.load(f)
-
-dek = bytes(conf["ecryption_key"], 'utf-8')
-BLOCK_SIZE = 16
-aes = AES.new(dek, AES.MODE_ECB)
-
 
 @pytest.mark.asyncio
 async def test_can_encrypt_password_with_long_password():
     # given : 비정상적인 비밀번호(매우 긴 패스워드)
     long_password = ORI_PASSWORD * 10
-    assert len(long_password) > 100
-
-    encoding_password = pad(long_password.encode(), BLOCK_SIZE)
 
     # when : 암호화
-    encrypt_password = aes.encrypt(encoding_password)
-    base64_password = base64.b64encode(encrypt_password)
+    encrypt_password = CipherManager().encrypt_password(long_password)
 
     # then : base64인코딩 + 암호화된 비밀번호
-    assert isinstance(base64_password, bytes)
+    assert isinstance(encrypt_password, bytes)
 
     # when : 복호화
-    decrypt_password = aes.decrypt(base64.b64decode(base64_password))
-    origin_password = unpad(decrypt_password, BLOCK_SIZE).decode()
+    origin_password = CipherManager().decrypt_password(encrypt_password)
 
     # then : 비밀번호 동일 확인
     assert origin_password == long_password
@@ -51,18 +28,13 @@ async def test_can_encrypt_password_with_short_password():
     short_password = "a"
     assert len(short_password) == 1
 
-    encoding_password = pad(short_password.encode(), BLOCK_SIZE)
-
-    # when : 암호화
-    encrypt_password = aes.encrypt(encoding_password)
-    base64_password = base64.b64encode(encrypt_password)
+    encrypt_password = CipherManager().encrypt_password(short_password)
 
     # then : base64인코딩 + 암호화된 비밀번호
-    assert isinstance(base64_password, bytes)
+    assert isinstance(encrypt_password, bytes)
 
     # when : 복호화
-    decrypt_password = aes.decrypt(base64.b64decode(base64_password))
-    origin_password = unpad(decrypt_password, BLOCK_SIZE).decode()
+    origin_password = CipherManager().decrypt_password(encrypt_password)
 
     # then : 비밀번호 동일 확인
     assert origin_password == short_password
@@ -71,18 +43,14 @@ async def test_can_encrypt_password_with_short_password():
 @pytest.mark.asyncio
 async def test_can_encrypt_decrypt_password():
     # given : 정상적인 비밀번호
-    encoding_password = pad(ORI_PASSWORD.encode(), BLOCK_SIZE)
-
     # when : 암호화
-    encrypt_password = aes.encrypt(encoding_password)
-    base64_password = base64.b64encode(encrypt_password)
+    encrypt_password = CipherManager().encrypt_password(ORI_PASSWORD)
 
     # then : base64인코딩 + 암호화된 비밀번호
-    assert isinstance(base64_password, bytes)
+    assert isinstance(encrypt_password, bytes)
 
     # when : 복호화
-    decrypt_password = aes.decrypt(base64.b64decode(base64_password))
-    origin_password = unpad(decrypt_password, BLOCK_SIZE).decode()
+    origin_password = CipherManager().decrypt_password(encrypt_password)
 
     # then : 비밀번호 동일 확인
     assert origin_password == ORI_PASSWORD

4941d91b1375e4f1c413f0cd045f3c9243819f29
robert-min <robertmin522@gmail.com>
2024-01-30 13:14:25 +0900

    test: ADD apps/account/test_service login_user test code.

--- tests/apps/account/test_service.py
+++ tests/apps/account/test_service.py
@@ -1,6 +1,5 @@
 import pytest
 from src.libs.exception import UserError
-from src.libs.validator import ApiValidator
 from src.libs.db_manager import PostgreManager
 from src.apps.account.schema import UserSignupPayload
 from src.apps.account.repository import AccountRepository
@@ -34,14 +33,13 @@ def session():
 @pytest.mark.asyncio
 async def test_account_service_cannot_signup_user_with_existence_user(mockup, session):
     # given : 이미 가입된 Email
-    mockup = mockup.model_dump()
-    result = AccountRepository.insert_user_account(session, mockup)
+    result = AccountService.signup_user(session, mockup)
     assert result == EMAIL
 
     # then : UserError 중복된 이메일
     with pytest.raises(UserError):
         # when : 중복된 Email 여부 확인
-        ApiValidator.check_user_existence(session, mockup["email"])
+        AccountService.signup_user(session, mockup)
 
     result = AccountRepository.delete_user_account(session, EMAIL)
     assert result == EMAIL
@@ -56,6 +54,38 @@ async def test_account_service_can_signup_user_with_valid(mockup, session):
 
     # then : 요청한 이메일 동일
     assert result == EMAIL
+
+
+@pytest.mark.order(3)
+@pytest.mark.asyncio
+async def test_account_service_cannot_login_with_no_signup_account():
+    # given : 가입되지 않은 계정 로그인
+    
+    # when : 사용자 로그인 요청
+    
+    # then : UserError 반환
+    pass
+
+
+@pytest.mark.order(3)
+@pytest.mark.asyncio
+async def test_account_service_cannot_login_with_wrong_password():
+    # given : 유효한 로그인 정보 + 잘못된 비밀번호
+    
+    # when : 사용자 로그인 요청
+    
+    # then : UserError 반환
+    pass
+
+
+@pytest.mark.order(4)
+@pytest.mark.asyncio
+async def test_account_service_can_login_with_valid(session):
+    # given : 유효한 로그인 정보
+
+    # when : 사용자 로그인 요청
+
+    # then : email, toeken 반환
 
     result = AccountRepository.delete_user_account(session, EMAIL)
     assert result == EMAIL

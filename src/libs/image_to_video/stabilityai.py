578cf40ee47c5354aae5855ad5600efea93f62e9
robert-min <robertmin522@gmail.com>
2024-02-05 19:27:59 +0900

    feature: ADD VideoManager.get_generated_video

--- src/libs/image_to_video/stabilityai.py
+++ src/libs/image_to_video/stabilityai.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 from PIL import Image
 
 
@@ -17,7 +18,9 @@ class VideoManager:
 
     def generate_video_content(self):
         resize_image_path = self.resize_image("origin_img.jpg")
-        self.post_generated_video(resize_image_path)
+        response = self.post_generated_video(resize_image_path)
+        generated_id = response.json()["id"]
+        origin_video_path = self.get_generated_video(generated_id, "origin_video.mp4")
 
     def resize_image(self, origin_img_name: str):
         # base image
@@ -32,9 +35,8 @@ class VideoManager:
         return resized_image_path
 
     def post_generated_video(self, resized_img_name: str):
-        # resized_image_path
         resize_image_path = os.path.abspath(os.path.join(self.user_folder_path, resized_img_name))
-        
+
         # request generated video to stability ai
         response = requests.post(
             "https://api.stability.ai/v2alpha/generation/image-to-video",
@@ -52,3 +54,30 @@ class VideoManager:
         )
         # TODO : 예외 처리 필요
         return response
+
+    def get_generated_video(self, generated_id: str, origin_video_name: str):
+        origin_video_path = os.path.abspath(os.path.join(self.user_folder_path, origin_video_name))
+
+        flag = 0
+        while flag != 200:
+            response = requests.get(
+                f"https://api.stability.ai/v2alpha/generation/image-to-video/result/{generated_id}",
+                headers={
+                    'Accept': None, # Use 'application/json' to receive base64 encoded JSON
+                    'authorization': self.token
+                },
+            )
+
+            # flag에 status code 할당
+            flag = response.status_code
+            if flag == 202:
+                print("Generation in-progress... automatically try again after 3 sec.")
+                time.sleep(3)
+            elif flag == 200:
+                print("Generation complete")
+                with open(origin_video_path, 'wb') as file:
+                    file.write(response.content)
+                print(f"video Saved at {origin_video_path}")
+            else:
+                raise Exception("Non-200 response : " + str(response.json()))
+        return origin_video_path
--- tests/libs/image_to_video/test_stabilityai.py
+++ tests/libs/image_to_video/test_stabilityai.py
@@ -20,32 +20,32 @@ SEED = 0
 CFG_SCALE = 2.5
 MOTION_BUCKET_ID=40
 GENERATED_ID="bb8216b8ac65f2d91ed5ceedb496d24b63e2be55f389582080619b22d7694b8d"
-GENERATE_ORIGIN_VIDEO_PATH = os.path.abspath(os.path.join(test_img_folder_path, "generate_origin_video.mp4"))
+GENERATE_ORIGIN_VIDEO_PATH = os.path.abspath(os.path.join(test_img_folder_path, "origin_video.mp4"))
 REVERSED_VIDEO_PATH = os.path.abspath(os.path.join(test_img_folder_path, "reversed_video.mp4"))
 SLOW_VIDEO_PATH = os.path.abspath(os.path.join(test_img_folder_path, "slow_video.mp4"))
 
 
-@pytest.mark.order(1)
-@pytest.mark.asyncio
-async def test_can_resize_image():
-    # given : 유효한 이미지
-    # when : 이미지 사이즈 변경 요청
-    resize_image_path = VideoManager(test_img_folder_path).resize_image("origin_img.jpg")
+# @pytest.mark.order(1)
+# @pytest.mark.asyncio
+# async def test_can_resize_image():
+#     # given : 유효한 이미지
+#     # when : 이미지 사이즈 변경 요청
+#     resize_image_path = VideoManager(test_img_folder_path).resize_image("origin_img.jpg")
 
-    # then : 이미지 저장 파일 확인
-    assert os.path.exists(resize_image_path)
+#     # then : 이미지 저장 파일 확인
+#     assert os.path.exists(resize_image_path)
 
 
-@pytest.mark.order(2)
-@pytest.mark.asyncio
-async def test_can_request_generate_video():
-    # given : 유효한 토큰 + 이미지
-    # RESIZED_IMAGE_PATH = os.path.abspath(os.path.join(test_img_folder_path, "resized_img.jpg"))
-    # when : 비디오 생성 요청
-    response = VideoManager(test_img_folder_path).post_generated_video("resized_img.jpg")
-    print(response.json())
-    assert response.status_code == 200
-    assert len(response.json()["id"]) > 0
+# @pytest.mark.order(2)
+# @pytest.mark.asyncio
+# async def test_can_request_generate_video():
+#     # given : 유효한 토큰 + 이미지
+#     # RESIZED_IMAGE_PATH = os.path.abspath(os.path.join(test_img_folder_path, "resized_img.jpg"))
+#     # when : 비디오 생성 요청
+#     response = VideoManager(test_img_folder_path).post_generated_video("resized_img.jpg")
+#     print(response.json())
+#     assert response.status_code == 200
+#     assert len(response.json()["id"]) > 0
 
 
 # def test_cannot_request_generate_video_with_non_token():
@@ -73,58 +73,39 @@ async def test_can_request_generate_video():
 #     assert response.status_code == 404
 
 
-def test_cannnot_request_generate_video_with_non_image():
-    # given : 유효한 토큰 + 이미지 없음
-    no_image_path = os.path.abspath(os.path.join(test_img_path, "no_image.jpg"))
+# def test_cannnot_request_generate_video_with_non_image():
+#     # given : 유효한 토큰 + 이미지 없음
+#     no_image_path = os.path.abspath(os.path.join(test_img_path, "no_image.jpg"))
 
-    # when : 비디오 생성 요청
-    # then : 이미지 없음 메시지
-    with pytest.raises(FileNotFoundError):
-        response = requests.post(
-            "https://api.stability.ai/v2alpha/generation/image-to-video",
-            headers={
-                "authorization": "Bearer " + TOKEN_KEY,
-            },
-            data={
-                "seed": SEED,
-                "cfg_scale": CFG_SCALE,
-                "motion_bucket_id": MOTION_BUCKET_ID
-            },
-            files={
-                "image": ("file", open(no_image_path, "rb"), "image/png")
-            },
-        )
-        assert response.status_code == 200
-
-# # TODO : pytest.mark.order 추가!!
-# def test_can_get_generated_video():
-#     # given : 생성된 비디오 요청 id
-#     # when : 비디오 전달 요청
-#     flag = 0
-#     while flag != 200:
-#         response = requests.get(
-#             f"https://api.stability.ai/v2alpha/generation/image-to-video/result/{GENERATED_ID}",
+#     # when : 비디오 생성 요청
+#     # then : 이미지 없음 메시지
+#     with pytest.raises(FileNotFoundError):
+#         response = requests.post(
+#             "https://api.stability.ai/v2alpha/generation/image-to-video",
 #             headers={
-#                 'Accept': None, # Use 'application/json' to receive base64 encoded JSON
-#                 'authorization': TOKEN_KEY
+#                 "authorization": "Bearer " + TOKEN_KEY,
+#             },
+#             data={
+#                 "seed": SEED,
+#                 "cfg_scale": CFG_SCALE,
+#                 "motion_bucket_id": MOTION_BUCKET_ID
+#             },
+#             files={
+#                 "image": ("file", open(no_image_path, "rb"), "image/png")
 #             },
 #         )
-        
-#         # flag에 status code 할당
-#         flag = response.status_code
-#         if flag == 202:
-#             print("Generation in-progress... automatically try again after 3 sec.")
-#             time.sleep(3)
-#         elif flag == 200:
-#             print("Generation complete")
-#             with open(GENERATE_ORIGIN_VIDEO_PATH, 'wb') as file:
-#                 file.write(response.content)
-#             print(f"video Saved at {GENERATE_ORIGIN_VIDEO_PATH}")
-#         else:
-#             raise Exception("Non-200 response : " + str(response.json()))
+#         assert response.status_code == 200
 
-#     # then : 비디오 전달 확인
-#     assert os.path.exists(GENERATE_ORIGIN_VIDEO_PATH)
+
+@pytest.mark.order(3)
+def test_can_get_generated_video():
+    # given : 생성된 비디오 요청 id
+    # when : 비디오 전달 요청
+    generated_origin_path = VideoManager(
+        test_img_folder_path).get_generated_video(GENERATED_ID, "origin_video.mp4")
+
+    # then : 비디오 전달 확인
+    assert os.path.exists(generated_origin_path)
 
 # def reverse_generated_video(output_video: cv2.VideoWriter, reverse: bool = False):
 #     # 비디오 캡쳐 객체 생성

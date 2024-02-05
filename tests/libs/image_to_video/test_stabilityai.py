4dfb4c64a495c7da2943a3c36c0822e43a8e84d9
robert-min <robertmin522@gmail.com>
2024-02-05 15:17:25 +0900

    test: ADD image_to_video/stabilityai test scenario.

---
+++ tests/libs/image_to_video/test_stabilityai.py
@@ -0,0 +1,46 @@
+import pytest
+
+
+def test_can_request_generate_video():
+    # given : 유효한 토큰
+
+    # when : 비디오 생성 요청
+
+    # then : 생성 요청 확인
+    pass
+
+
+def test_cannot_request_generate_video():
+    # given : 토큰 부족
+
+    # when : 비디오 생성 요청
+
+    # then : 토큰 부족 메시지
+    pass
+
+
+def test_can_get_generated_video():
+    # given : 생성된 비디오 요청 id
+
+    # when : 비디오 전달 요청
+
+    # then : 비디오 전달 확인
+    pass
+
+
+def test_can_reverse_generated_video():
+    # given : 유효한 비디오 파일
+
+    # when : 비디오 파일 역 재생 및 연결
+
+    # then : 비디오 파일 유효성 확인
+    pass
+
+
+def test_can_slow_generated_video():
+    # given : 유효한 비디오 파일
+
+    # when : 비디오 파일 느리게
+
+    # then : 비디오 파일 유효성 확인
+    pass
